6727dcce07ff6c19ed9f0e34dcc0393ecdec6868
neerajv-14 <120648151+neerajv-14@users.noreply.github.com>
2026-09-13 01:14:44 +0530

    Add: counting-bits - submission-1

---
+++ Data Structures & Algorithms/counting-bits/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n+1);
+
+        ans[0] = 0;
+
+        // int si=1,ptr=1;
+
+        // while(ptr<=n){
+
+        //     for(int i=0;i<si && ptr<=n;i++){
+        //         ans[ptr] = 1 + ans[i];
+        //         ptr++;
+        //     }
+
+        //     si*= 2;
+        // }
+
+        for(int i=1;i<=n;i++){
+            ans[i] = ans[i>>1] + (i & 1);
+        }
+        return ans;
+        
+
+    }
+};
